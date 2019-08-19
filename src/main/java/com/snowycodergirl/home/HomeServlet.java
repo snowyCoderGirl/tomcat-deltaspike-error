61537282c231c070de2625e560b13168ada4d97d
Rachel Gotwalt <rachel.gotwalt@berwickinsurance.com>
2019-08-19 11:26:32 -0700

    Added basic servlet for testing

---
+++ src/main/java/com/snowycodergirl/home/HomeServlet.java
@@ -0,0 +1,18 @@
+package com.snowycodergirl.home;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/home")
+public class HomeServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().println("Hello World");
+    }
+
+}
